d6bb498ecf06c078e79d4bd34b2673e6a80f0b07
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-04-19 23:19:03 -0400

    Posts showing

---
+++ app/routes/posts.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.$.getJSON('https://www.reddit.com/r/funny.json')
+      .then((result) => {
+        return result.data.children;
+      });
+  }
+});
+
